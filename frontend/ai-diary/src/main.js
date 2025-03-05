051d4db7ca69469cb9048f9f03a9b299c05a5802
Eskioi <159459093+Eskioi@users.noreply.github.com>
2025-03-05 10:01:29 +0100

    remove axios

--- frontend/ai-diary/src/main.js
+++ frontend/ai-diary/src/main.js
@@ -1,10 +1,8 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './components/router';
-import axios from 'axios';
 
 const app = createApp(App);
-axios.defaults.baseURL = 'http://localhost:8080';
 
 app.use(router);
 app.mount('#app');
